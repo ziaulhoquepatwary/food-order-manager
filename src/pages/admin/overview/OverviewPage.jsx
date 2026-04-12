3c7f7c12d1857a1d5ce2e32d08ae6bad3f267b8b
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-04-12 16:00:34 +0600

    OverviewPage section design done

--- src/pages/admin/overview/OverviewPage.jsx
+++ src/pages/admin/overview/OverviewPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import StatCard from './StatCard'
+import RecentOrdersTable from './RecentOrdersTable'
+import PendingCookers from './PendingCookers'
+import TopSellingItems from './TopSellingItems'
 
 function OverviewPage() {
     return (
-        <div className="flex flex-col gap-6">OverviewPage</div>
+        <div className="flex flex-col gap-6">
+            <StatCard />
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <RecentOrdersTable />
+                <PendingCookers />
+            </div>
+
+            <TopSellingItems />
+        </div>
     )
 }
 
